01054a39337bbb2234f6d50cc1128f7281332f5c
Elliott Smiggen <esmiggen@gmail.com>
2012-10-31 10:42:25 -0400

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeFlareSplash::Application.config.secret_token = '6f23d7887ed3c5796887116064801876d2ce0f55cb97c317c1ce5e98d64e14c8cda64c1b29195effc8f5c6d916e24fe401947093ddec58fb707a20307366620b'
