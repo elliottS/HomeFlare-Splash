fe99c363fd4a4a6b894ac142c58db1440570cebc
Elliott Smiggen <esmiggen@gmail.com>
2012-11-02 17:25:03 -0400

    Design re-vamp
    +- More changes to come, pushing temporarily for updates when I get home

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module HomeFlareSplash
   class Application < Rails::Application
+
+    # Font Awesome path addition
+    config.assets.paths << Rails.root.join("app", "assets", "font")
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -58,5 +62,7 @@ module HomeFlareSplash
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
   end
 end
