d1df1d0bba83f5142b1ae00b1606768ddfdae56d
Elliott Smiggen <esmiggen@gmail.com>
2012-11-06 17:05:28 -0500

    Tutorials updates, functionality changes
    +- Put js in it's own file
    +- content changes
    +- ToC is now affixed when scrolling past conversations, will need to
    accommodate for different resolutions however

---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+    $(".pop_out").animate({left:"100px"}, 2000);
+
+    $(window).bind("resize.browsersize", function() {
+                    
+        var $tab = $('.nav-tabs');
+            windowHalf = $(window).height() / 2;
+                        
+        $(window).scroll(function(){
+                      
+            if ($(window).scrollTop() > 1800 ){
+                          
+                $tab.css({ position:'fixed', top:'11%', left: '207px'});
+                
+                              
+            }  else {
+                $tab.css({ position:'relative', top:'0', left: '0' });   
+            }
+            
+        });  
+                      
+    }).trigger("resize.browsersize");
+  });
