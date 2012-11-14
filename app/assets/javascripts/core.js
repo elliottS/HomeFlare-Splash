5ce5416d1f6a84018481d67bbc2379412a2a1c90
Elliott Smiggen <esmiggen@gmail.com>
2012-11-14 11:07:12 -0500

    Chapters ToC highlights current location
    +- added a better script to catch vertical event locations and apply a
    class to the current location in the Chapters ToC

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -18,26 +18,57 @@ $(document).ready(function() {
     $(window).bind("resize.browsersize", function() {               
         var $tab = $('.nav-tabs');    
 
-        $(window).scroll(function(){      
-            if ($(window).scrollTop() > 2300 ){     //Big resolution monitors
+        $(window).scroll(function(){   
+            if ($(window).scrollTop() > 1300 ){    
+                $(".nav-tabs li").eq(0).removeClass("activeChapter");
+                $(".nav-tabs li").eq(1).addClass("activeChapter");
+                $(".nav-tabs li").eq(2).removeClass("activeChapter");  
 
-                $(".nav-tabs li").eq(1).css( 'background-color', "#A73A37");  
-                $(".nav-tabs li a").eq(0).css( 'color', "#fff");  
+                if ($(window).scrollTop() > 1700 ){
+                    $(".nav-tabs li").eq(1).removeClass("activeChapter");
+                    $(".nav-tabs li").eq(2).addClass("activeChapter");
+                    $(".nav-tabs li").eq(3).removeClass("activeChapter");
+                }
 
-                var windowWidth = $(window).width();
-                if (windowWidth > 1350) {           //Medium resolution monitors
-                    $tab.css({ position:'fixed', top:'11%', left: '20.25%'});
-                } else if (windowWidth > 1000) {    //Smaller resolution monitors
-                    $tab.css({ position:'fixed', top:'11%', left: '8%'});
+                if ($(window).scrollTop() > 2300 ){     //Big resolution monitors
+                    var windowWidth = $(window).width();
+                    if (windowWidth > 1350) {           //Medium resolution monitors
+                        $tab.css({ position:'fixed', top:'11%', left: '20.25%'});
+                    } else if (windowWidth > 1000) {    //Smaller resolution monitors
+                        $tab.css({ position:'fixed', top:'11%', left: '8%'});
+                    } else {
+                        $tab.css({ position:'fixed', top:'11%', left: '0%'});
+                    } 
+                    
+                    if ($(window).scrollTop() > 4000 ) {
+                        $(".nav-tabs li").eq(2).removeClass("activeChapter");
+                        $(".nav-tabs li").eq(3).addClass("activeChapter");
+                        $(".nav-tabs li").eq(4).removeClass("activeChapter");
+                    }
+                    if ($(window).scrollTop() > 5000 ) {
+                        $(".nav-tabs li").eq(3).removeClass("activeChapter");
+                        $(".nav-tabs li").eq(4).addClass("activeChapter");
+                        $(".nav-tabs li").eq(5).removeClass("activeChapter");
+                    }
+                    if ($(window).scrollTop() > 6000 ) {
+                        $(".nav-tabs li").eq(4).removeClass("activeChapter");
+                        $(".nav-tabs li").eq(5).addClass("activeChapter");
+                        $(".nav-tabs li").eq(6).removeClass("activeChapter");
+                    }
+                    if ($(window).scrollTop() > 6700 ) {
+                        $(".nav-tabs li").eq(5).removeClass("activeChapter");
+                        $(".nav-tabs li").eq(6).addClass("activeChapter");
+                        $(".nav-tabs li").eq(7).removeClass("activeChapter");
+                    }
+                    if ($(window).scrollTop() > 7400 ) {
+                        $(".nav-tabs li").eq(6).removeClass("activeChapter");
+                        $(".nav-tabs li").eq(7).addClass("activeChapter");
+                        $(".nav-tabs li").eq(8).removeClass("activeChapter");
+                    }
                 } else {
-                    $tab.css({ position:'fixed', top:'11%', left: '0%'});
-                }          
-            }  else if ($(window).scrollTop() > 3000 ){
-                $(".nav-tabs li").eq(1).css( 'background-color', "#A73A37");  
-                $(".nav-tabs li a").eq(0).css( 'color', "#fff");
-            } else {
-                $tab.css({ position:'relative', top:'0', left: '0' });   
-            }
+                    $tab.css({ position:'relative', top:'0', left: '0' });   
+                }
+            } 
         });             
     }).trigger("resize.browsersize");
   });
