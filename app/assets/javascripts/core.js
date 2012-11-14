e48d0bc740b7de71279515b1f145a20d8b6d3df7
Elliott Smiggen <esmiggen@gmail.com>
2012-11-14 14:10:08 -0500

    Changed bg image
    +Changed bg image to accommodate larger resolution monitors

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -20,50 +20,43 @@ $(document).ready(function() {
 
         $(window).scroll(function(){   
             if ($(window).scrollTop() > 1300 ){    
-                $(".nav-tabs li").eq(0).removeClass("activeChapter");
+                $(".nav-tabs li").removeClass("activeChapter");
                 $(".nav-tabs li").eq(1).addClass("activeChapter");
-                $(".nav-tabs li").eq(2).removeClass("activeChapter");  
 
                 if ($(window).scrollTop() > 1700 ){
-                    $(".nav-tabs li").eq(1).removeClass("activeChapter");
+                    $(".nav-tabs li").removeClass("activeChapter");
                     $(".nav-tabs li").eq(2).addClass("activeChapter");
-                    $(".nav-tabs li").eq(3).removeClass("activeChapter");
                 }
 
-                if ($(window).scrollTop() > 2300 ){     //Big resolution monitors
+                if ($(window).scrollTop() > 2290 ){     //Big resolution monitors
                     var windowWidth = $(window).width();
                     if (windowWidth > 1350) {           //Medium resolution monitors
-                        $tab.css({ position:'fixed', top:'11%', left: '20.25%'});
+                        $tab.css({ position:'fixed', top:'8%', left: '20.35%'});
                     } else if (windowWidth > 1000) {    //Smaller resolution monitors
-                        $tab.css({ position:'fixed', top:'11%', left: '8%'});
+                        $tab.css({ position:'fixed', top:'8%', left: '8%'});
                     } else {
-                        $tab.css({ position:'fixed', top:'11%', left: '0%'});
+                        $tab.css({ position:'fixed', top:'8%', left: '0%'});
                     } 
                     
                     if ($(window).scrollTop() > 4000 ) {
-                        $(".nav-tabs li").eq(2).removeClass("activeChapter");
+                        $(".nav-tabs li").removeClass("activeChapter");
                         $(".nav-tabs li").eq(3).addClass("activeChapter");
-                        $(".nav-tabs li").eq(4).removeClass("activeChapter");
                     }
                     if ($(window).scrollTop() > 5000 ) {
-                        $(".nav-tabs li").eq(3).removeClass("activeChapter");
+                        $(".nav-tabs li").removeClass("activeChapter");
                         $(".nav-tabs li").eq(4).addClass("activeChapter");
-                        $(".nav-tabs li").eq(5).removeClass("activeChapter");
                     }
                     if ($(window).scrollTop() > 6000 ) {
-                        $(".nav-tabs li").eq(4).removeClass("activeChapter");
+                        $(".nav-tabs li").removeClass("activeChapter");
                         $(".nav-tabs li").eq(5).addClass("activeChapter");
-                        $(".nav-tabs li").eq(6).removeClass("activeChapter");
                     }
                     if ($(window).scrollTop() > 6700 ) {
-                        $(".nav-tabs li").eq(5).removeClass("activeChapter");
+                        $(".nav-tabs li").removeClass("activeChapter");
                         $(".nav-tabs li").eq(6).addClass("activeChapter");
-                        $(".nav-tabs li").eq(7).removeClass("activeChapter");
                     }
                     if ($(window).scrollTop() > 7400 ) {
-                        $(".nav-tabs li").eq(6).removeClass("activeChapter");
+                        $(".nav-tabs li").removeClass("activeChapter");
                         $(".nav-tabs li").eq(7).addClass("activeChapter");
-                        $(".nav-tabs li").eq(8).removeClass("activeChapter");
                     }
                 } else {
                     $tab.css({ position:'relative', top:'0', left: '0' });   
